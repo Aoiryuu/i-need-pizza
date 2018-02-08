7b13e5e0572118ed87fbda0a0af70fb07bca36db
Aoiryuu <19joroka@waipahuh.k12.hi.us>
2018-02-07 14:40:28 -1000

    ARG

--- js/pizza.js
+++ js/pizza.js
@@ -4,6 +4,46 @@
 * Each object will have two nested objects ex.(pizza & toppings)
 * Keys with more than one value should be an array of strings ex. Veggies
 */
+var order11821 = {
+	Name: "James Franko",
+	Address: "2155 E Lake Dr",
+	City: "Seattle",
+	State: "WA",
+	ZipCode: 11310,
+	Pizza:{
+	  Crust: "thin",
+	   PizzaSauce: "robust marinara",
+	   Cheese: "regular",
+	   Toppings:{
+	        Veggies: ["lives","green peppers"],
+	        Meats: ["pepperoni", "ham", "sausage"]}},
+SideItems: ["breadsticks", "bbq wings" + 6 + "piece"],
+Sauces: "ranch",
+SpecialInstructions: "please cut into squares",
+PaymentType: "cash",
+StoreId: 112,
+}
+console.log(order11821)
+
+var order11372 = {
+	Customer Name: Justin Bieber
+Address: 1801 W Rodeo Dr
+City: Beverly Hills
+State: CA
+Zip Code: 91833
+Pizza:
+  Crust: stuffed crust
+  Pizza Sauce: creamy garlic alfredo
+  Cheese: double
+  Toppings:
+    Veggies: onion, anchovies, mushrooms
+    Meats: chicken, bacon
+Side Items: double fudge brownie dessert
+Sauces: bbq, garlic sauce
+Special Instructions: call 815 at the gate
+Payment Type: credit card
+Store Id: 382
+}
 
 
 /*
